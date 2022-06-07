803673ccef385526fb416ed326418c4b5c1df7f7
sc30gsw <haidoushi875@gmail.com>
2022-06-08 07:33:06 +0900

    管理サイトにEntryモデルを登録

--- learning_log/learning_los/admin.py
+++ learning_log/learning_los/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Topic
+from .models import Topic, Entry
 
 # Topicモデルを管理サイトに登録
 admin.site.register(Topic)
+# Entryモデルを管理サイトに登録
+admin.site.register(Entry)
