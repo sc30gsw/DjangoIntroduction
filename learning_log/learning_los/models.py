92b28e2fbbdf47b29071fbe510786b9fc64ab7fd
sc30gsw <haidoushi875@gmail.com>
2022-06-08 06:27:19 +0900

    Topicモデルの作成

--- learning_log/learning_los/models.py
+++ learning_log/learning_los/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+  """ユーザーが学んでいるトピックを表すモデル
+
+  Args:
+      models (_type_): Model
+
+  Returns:
+      _type_: Topic
+  """
+  
+  text = models.CharField(max_length=200)
+  data_added = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    """モデルの文字列表現を返す
+
+    Returns:
+        str: Topic.text
+    """
+    return self.text
