e1d4f0bfddcfd674de1fb0d6af5b644ef214aa48
sc30gsw <haidoushi875@gmail.com>
2022-06-08 07:01:26 +0900

    Topicテーブルの作成

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My Application
+    'learning_los',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
