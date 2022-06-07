46724752cb749149dc6b354f907dd50f8997f01a
sc30gsw <haidoushi875@gmail.com>
2022-06-08 08:18:46 +0900

    ホーム画面のViewの作成

--- learning_log/learning_los/views.py
+++ learning_log/learning_los/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  """学習ノートのホームページに遷移する処理
+
+  Args:
+      request (_type_): request
+
+  Returns:
+      _type_: index.html
+  """
+  return render(request, 'learning_los/index.html')
