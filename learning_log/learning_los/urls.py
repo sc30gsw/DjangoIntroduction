11f1d708f67a37c346bda82287e53a35e3673378
sc30gsw <haidoushi875@gmail.com>
2022-06-09 07:33:36 +0900

    新規トピック追加画面・処理の実装

--- learning_log/learning_los/urls.py
+++ learning_log/learning_los/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
   path('topics/', views.topics, name="topics"),
   # トピック詳細を表示するページ
   path('topics/<int:topic_id>/', views.topic, name="topic"),
+  # 新規トピック作成ページのURL
+  path('new_topic/', views.new_topic, name="new_topic"),
 ]
--- learning_log/learning_los/views.py
+++ learning_log/learning_los/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
   """学習ノートのホームページに遷移する処理
@@ -45,3 +46,28 @@ def topic(request, topic_id):
     # htmlに渡すコンテキストに個別トピックと記事内容を渡す
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_los/topic.html', context)
+
+def new_topic(request):
+    """新規トピック作成画面に遷移し、トピックを作成する処理
+
+    Args:
+        request (_type_): request
+
+    Returns:
+        _type_: new_topic.html
+    """
+    # リクエストがPOSTでない場合
+    if request.method != 'POST':
+        # データを送信しないため空のフォームを生成する
+        form = TopicForm()
+    # リクエストがPOSTの場合
+    else:
+        # POSTでデータが送信されるためフォームにデータを渡す
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_los:topics')
+        
+    # 空または無効のフォームを表示する
+    context = {'form': form}
+    return render(request, 'learning_los/new_topic.html', context)
