36b56559dfc191d3175ada742b681ee5579262c2
sc30gsw <haidoushi875@gmail.com>
2022-06-10 08:02:48 +0900

    ユーザー新規登録機能の実装

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -3,8 +3,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
   # デフォルトの認証URLを取り込む
   path('', include('django.contrib.auth.urls')),
+  # ユーザー登録ページ
+  path('register/', views.register, name='register'),
 ]
