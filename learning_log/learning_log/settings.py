7df6f973b17f8322c41aeb6a65a724e8c4d0cef2
sc30gsw <haidoushi875@gmail.com>
2022-06-10 08:07:59 +0900

    未ログインユーザーとログインユーザーで処理を変更する

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 自分の設定
+LOGIN_URL = 'users:login'
--- learning_log/learning_los/views.py
+++ learning_log/learning_los/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import EntryForm, TopicForm
@@ -14,6 +15,7 @@ def index(request):
   """
   return render(request, 'learning_los/index.html')
 
+@login_required
 def topics(request):
     """すべてのトピックを取得し、トピック一覧に遷移する処理
 
