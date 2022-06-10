c1e0f134ed2f020fc5448ff56c7f7a9675cf6925
sc30gsw <haidoushi875@gmail.com>
2022-06-11 08:55:16 +0900

    データ取得時に例外の判定を行うよう変更

--- learning_log/learning_los/views.py
+++ learning_log/learning_los/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -44,7 +44,7 @@ def topic(request, topic_id):
         _type_: topic.html
     """
     # id指定したトピックを取得する
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # トピックが現在のユーザーが所持するものであることを確認する
     check_topic_owner(topic.owner, request.user)
 
